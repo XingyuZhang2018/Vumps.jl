9dfcac141c82f6df6af9f9141a627900518de6be
XingyuZhang2018 <xinzaijiuhao@gmail.com>
2025-02-10 23:09:56 +0100

    structure array

--- src/vumpsruntime.jl
+++ src/vumpsruntime.jl
@@ -1,7 +1,7 @@
 @kwdef mutable struct VUMPS
     ifupdown::Bool = true
     ifdownfromup::Bool = false
-    ifsimple_eig::Bool = false
+    ifsimple_eig::Bool = Defaults.ifsimple_eig
     tol::Float64 = Defaults.tol
     maxiter::Int = Defaults.maxiter
     miniter::Int = Defaults.miniter
@@ -16,8 +16,8 @@ function init_VUMPSRuntime(M, χ::Int, alg::VUMPS)
     A = initial_A(M, χ)
     AL, L, _ = left_canonical(A)
     R, AR, _ = right_canonical(AL)
-    _, FL = leftenv(AL, conj.(AL), M; alg)
-    _, FR = rightenv(AR, conj.(AR), M; alg)
+    _, FL = leftenv(AL, conj(AL), M; alg)
+    _, FR = rightenv(AR, conj(AR), M; alg)
     C = LRtoC(L, R)
     return VUMPSRuntime(AL, AR, C, FL, FR)
 end
@@ -25,17 +25,21 @@ end
 _down_m(m::leg4) = permutedims(conj(m), (1,4,3,2))
 _down_m(m::leg5) = permutedims(conj(m), (1,4,3,2,5))
 _down_m(m::leg8) = permutedims(conj(m), (1,2,7,8,5,6,3,4))
-function _down_M(M)
+function _down_M(M::StructArray)
     Ni, Nj = size(M)
-    Md = Zygote.Buffer(M)
-    for j in 1:Nj, i in 1:Ni
-        ir = Ni + 1 - i
-        Md[i, j] = _down_m(M[ir, j])
+    pattern_d = copy(M.pattern)
+    Zygote.@ignore begin
+        @inbounds for i in 1:Ni, j in 1:Nj
+            ir = Ni + 1 - i
+            pattern_d[i, j] = M.pattern[ir, j]
+        end
     end
-    return copy(Md)
+    data_d = [_down_m(data) for data in M.data]
+    Md = StructArray(data_d, pattern_d)
+    return Md
 end
 
-function VUMPSRuntime(M, χ::Int, alg::VUMPS)
+function VUMPSRuntime(M::StructArray, χ::Int, alg::VUMPS)
     Ni, Nj = size(M)
 
     rtup = init_VUMPSRuntime(M, χ, alg)
@@ -56,7 +60,7 @@ function VUMPSRuntime(M, χ::Int, alg::VUMPS)
     end
 end
 
-function vumps_itr(rt::VUMPSRuntime, M, alg::VUMPS)
+function vumps_itr(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     t = Zygote.@ignore time()
 
     Zygote.@ignore alg.verbosity >= 2 && @info @sprintf("Start VUMPS iteration without AD...")
@@ -88,7 +92,7 @@ function vumps_itr(rt::VUMPSRuntime, M, alg::VUMPS)
     return rt
 end
 
-function leading_boundary(rt::VUMPSRuntime, M, alg::VUMPS)
+function leading_boundary(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     rtup = vumps_itr(rt, M, alg)
     if alg.ifupdown && alg.ifdownfromup
         Md = _down_M(M)
@@ -99,15 +103,15 @@ function leading_boundary(rt::VUMPSRuntime, M, alg::VUMPS)
     end
 end
 
-function VUMPSEnv(rt::VUMPSRuntime, M::Matrix, alg::VUMPS)
+function VUMPSEnv(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     @unpack AL, AR, C, FL, FR = rt
     AC = ALCtoAC(AL, C)
-    _, FLo =  leftenv(AL, conj.(AL), M, FL; ifobs = true, alg)
-    _, FRo = rightenv(AR, conj.(AR), M, FR; ifobs = true, alg)
+    _, FLo =  leftenv(AL, conj(AL), M, FL; ifobs = true, alg)
+    _, FRo = rightenv(AR, conj(AR), M, FR; ifobs = true, alg)
     return VUMPSEnv(AC, AR, AC, AR, FL, FR, FLo, FRo)
 end
 
-function leading_boundary(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M, alg::VUMPS)
+function leading_boundary(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M::StructArray, alg::VUMPS)
     rtup, rtdown = rt
     
     rtup = vumps_itr(rtup, M, alg)
@@ -121,7 +125,7 @@ function leading_boundary(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M, alg::VUMPS)
     return rtup, rtdown
 end
 
-function VUMPSEnv(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M, alg)
+function VUMPSEnv(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M::StructArray, alg)
     rtup, rtdown = rt
 
     ALu, ARu, Cu, FLu, FRu = rtup.AL, rtup.AR, rtup.C, rtup.FL, rtup.FR
@@ -130,19 +134,19 @@ function VUMPSEnv(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M, alg)
     ALd, ARd, Cd = rtdown.AL, rtdown.AR, rtdown.C
     ACd = ALCtoAC(ALd, Cd)
 
-    _, FLo =  leftenv(ALu, conj.(ALd), M, FLu; ifobs = true, alg)
-    _, FRo = rightenv(ARu, conj.(ARd), M, FRu; ifobs = true, alg)
+    _, FLo =  leftenv(ALu, conj(ALd), M, FLu; ifobs = true, alg)
+    _, FRo = rightenv(ARu, conj(ARd), M, FRu; ifobs = true, alg)
     return VUMPSEnv(ACu, ARu, ACd, ARd, FLu, FRu, FLo, FRo)
 end
 
-function vumps_step_power(rt::VUMPSRuntime, M, alg::VUMPS)
+function vumps_step_power(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     @unpack AL, C, AR, FL, FR = rt
     AC = ALCtoAC(AL,C)
     _, ACp = ACenv(AC, FL, M, FR; alg)
     _,  Cp =  Cenv( C, FL, FR; alg)
     ALp, ARp, _, _ = ACCtoALAR(ACp, Cp)
-    _, FL =  leftenv(AL, conj.(ALp), M, FL; alg)
-    _, FR = rightenv(AR, conj.(ARp), M, FR; alg)
+    _, FL =  leftenv(AL, conj(ALp), M, FL; alg)
+    _, FR = rightenv(AR, conj(ARp), M, FR; alg)
     _, ACp = ACenv(ACp, FL, M, FR; alg)
     _,  Cp =  Cenv( Cp, FL, FR; alg)
     ALp, ARp, errL, errR = ACCtoALAR(ACp, Cp)
@@ -151,11 +155,11 @@ function vumps_step_power(rt::VUMPSRuntime, M, alg::VUMPS)
     return VUMPSRuntime(ALp, ARp, Cp, FL, FR), err
 end
 
-function vumps_step_Hermitian(rt::VUMPSRuntime, M, alg::VUMPS)
+function vumps_step_Hermitian(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     @unpack AL, C, AR, FL, FR = rt
     AC = ALCtoAC(AL,C)
-    _, FL =  leftenv(AL, conj.(AL), M, FL; alg)
-    _, FR = rightenv(AR, conj.(AR), M, FR; alg)
+    _, FL =  leftenv(AL, conj(AL), M, FL; alg)
+    _, FR = rightenv(AR, conj(AR), M, FR; alg)
     _, AC = ACenv(AC, FL, M, FR; alg)
     _,  C =  Cenv( C, FL, FR; alg)
     AL, AR, errL, errR = ACCtoALAR(AC, C)
@@ -164,7 +168,7 @@ function vumps_step_Hermitian(rt::VUMPSRuntime, M, alg::VUMPS)
     return VUMPSRuntime(AL, AR, C, FL, FR), err
 end
 
-function fix_gauge_vumps_step(rt::VUMPSRuntime, M, alg::VUMPS)
+function fix_gauge_vumps_step(rt::VUMPSRuntime, M::StructArray, alg::VUMPS)
     rt′, err = vumps_step_Hermitian(rt, M, alg)
     ALu, ARu, Cu, FLu, FRu = rt.AL, rt.AR, rt.C, rt.FL, rt.FR
     ALd, ARd, Cd, FLd, FRd = rt′.AL, rt′.AR, rt′.C, rt′.FL, rt′.FR
